fab1f166508ac2ec92fda2ac106a04f4723215d9
Moisés Cachay <xpktro@gmail.com>
2017-04-29 11:00:43 -0500

    Class 6 material

--- safu/actividades/views.py
+++ safu/actividades/views.py
@@ -1,35 +1,18 @@
-from django.shortcuts import render
-from django.views import View
-
 from django.views.generic import TemplateView, ListView
 
 from actividades.models import Actividad
 
 
-# class HomeView(View):
-#     def get(self, request, *args, **kwargs):
-#         contexto = {
-#             'nombre': 'moises',
-#             'actividades': Actividad.objects.all()
-#         }
-#         return render(request, 'index.html', contexto)
-
-# class HomeView(TemplateView):
-#     template_name = 'index.html'
-#
-#     def get_context_data(self, **kwargs):
-#         return {
-#             'nombre': 'moises',
-#             'actividades': Actividad.objects.all()
-#         }
-
-class HomeView(ListView):
+class HomeView(TemplateView):
     template_name = 'index.html'
-    model = Actividad
-    context_object_name = 'actividades'
 
     def get_context_data(self, **kwargs):
-        # contexto = {'actividades': Actividad.objects.all()}
         contexto = super(HomeView, self).get_context_data(**kwargs)
-        contexto['nombre'] = 'moises'
+        contexto['actividades'] = Actividad.objects.all().order_by('-fecha')[:3]
         return contexto
+
+
+class ActividadesView(ListView):
+    template_name = 'actividades.html'
+    model = Actividad
+    context_object_name = 'actividades'
--- safu/safu/urls.py
+++ safu/safu/urls.py
@@ -4,10 +4,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from actividades.views import HomeView
+from actividades.views import HomeView, ActividadesView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view())
+    url(r'^actividades/$', ActividadesView.as_view(), name='actividades'),
+    url(r'^$', HomeView.as_view(), name='home')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
